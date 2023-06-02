050eb36db11f7f3aa4ad076dbddf037ba7422519
Fallrain1763 <liushuchen842@icloud.com>
2023-06-02 10:40:56 -0700

    set up

---
+++ cinematics.js
@@ -0,0 +1,23 @@
+class start extends Phaser.Scene {
+    constructor() {
+        super('start');
+    }
+    preload() {
+        this.load.path = './assets/';
+    }
+    create() {
+        this.add.text(640, 360, "Cinematics");
+    }
+    update() {
+
+    }
+}
+
+let config = {
+    type: Phaser.WEBGL,
+    width: 1280,
+    height: 720,
+    scene: [start]
+}
+
+let game = new Phaser.Game(config);
